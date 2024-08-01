140b7e79c8853e5b1f1e7d0997a38295262d2f47
Thabang Selowa <mohale20cobra@gmail.com>
2024-08-01 23:06:11 +0200

    Add singleton instance for bank branch

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
-
+const BranchInstancech = {
+    
+    name: "Absa",
+    code: 7955 ,
+    address: "",
+    number: "(012-0534465)"
+};
+console.log("Bank branch: " + BranchInstancech.name);
+console.log("Branch code: " + BranchInstancech.code);
+console.log("Branch address: " + BranchInstancech.address);
+console.log("Branch number: " + BranchInstancech.number);
 // 2. Define a class called `BankBranch` for managing branch information.
+class BankBranch {
 
 // 3. In the `BankBranch` class:
+constructor(name, code, address, number){
+
+
 //    - Create a constructor that takes `branchInfo` as a parameter.
+
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
 //    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
 //    - Return the `bankBranchInstance` whether it's newly created or existing.
-
+    }
+};
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
 
 // 5. In the usage section:
