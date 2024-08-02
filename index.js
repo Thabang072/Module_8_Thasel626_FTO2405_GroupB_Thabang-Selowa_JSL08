72596514f386c5b5a0d670fef2e7e02bf9089faf
Thabang Selowa <mohale20cobra@gmail.com>
2024-08-02 14:58:12 +0200

    Add methods  to the bankBranch for managing related information

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
-const BranchInstancech = {
+const branchInstancech = {
     
     name: "Absa",
     code: 7955 ,
     address: "",
     number: "(012-0534465)"
 };
-console.log("Bank branch: " + BranchInstancech.name);
-console.log("Branch code: " + BranchInstancech.code);
-console.log("Branch address: " + BranchInstancech.address);
-console.log("Branch number: " + BranchInstancech.number);
+console.log("Bank branch: " + branchInstancech.name);
+console.log("Branch code: " + branchInstancech.code);
+console.log("Branch address: " + branchInstancech.address);
+console.log("Branch number: " + branchInstancech.number);
 // 2. Define a class called `BankBranch` for managing branch information.
-class BankBranch {
+class bankBranch {
 
 // 3. In the `BankBranch` class:
 constructor(name, code, address, number){
 
-
+    
 //    - Create a constructor that takes `branchInfo` as a parameter.
 
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
 //    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
 //    - Return the `bankBranchInstance` whether it's newly created or existing.
     }
+    
+
 };
+this.name =  branch;
+console.log(this);
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
 
 // 5. In the usage section:
